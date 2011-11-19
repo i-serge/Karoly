7ef73c95b418891357e56278797cbf56ebf7e961
Sergio Morales Lopez <sergio@toolsnet.mx>
2011-11-18 23:24:46 -0600

    Added the tree-listing helper to the autoloaded helpers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','form','doctrine');
+$autoload['helper'] = array('url','form','doctrine','tree');
 
 
 /*
